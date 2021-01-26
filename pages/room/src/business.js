fe0bedc9baa8ef2262706dbe57590493dfc81dd9
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-26 20:48:27 -0300

    Criando as conexões ao server com as credentiais.

--- pages/room/src/business.js
+++ pages/room/src/business.js
@@ -1,25 +1,36 @@
 class Business {
-    constructor({ room, media, view }) {
+
+    constructor({ room, media, view, socketBuilder }) {
         this.media = media;
         this.room = room;
         this.view = view;
 
+        // se o usuario estiver conetado ele faz a build para o servidor que estar rodando por trás 
+        this.socketBuilder = socketBuilder
+            .setOnUserConnected(this.onUserConnected())
+            .build();
+
+        this.socketBuilder.emit('join-room', this.room, 'teste01')
         this.currentStream = {}
     }
 
+    // inicializa o controlador businnes com as dependencias e intanciando elas no app.js
     static initialize(deps) {
         const instance = new Business(deps);
         return instance._init();
     }
 
+    // aqui a função init anste de tudo pede permisão do usuario e se
+    // ele aceita ela roda a função para acessar a camera do navegador
     async _init() {
         this.currentStream = await this.media.getCamera();
-        // console.log('init!!', this.currentStream)
+        console.log('init!!', this.currentStream)
 
         this.addVideoStream('teste01');
 
     }
 
+    // A funçao captura o id do usuario apra renderizar as telas de videos de todos os usuarios na chamdada
     addVideoStream(userId, stream = this.currentStream) {
         const isCurrentId = false;
         this.view.renderVideo({
@@ -28,4 +39,18 @@ class Business {
             isCurrentId
         })
     }
+
+    // se tiver conecatdo ele retorna o id do usuario atraves do socket.io
+    onUserConnected = function () {
+        return userId => {
+            console.log('user connected', userId);
+        }
+    }
+
+    onUserDisconnected = function () {
+        return userId => {
+            console.log('user disconnected', userId);
+        }
+    }
+
 }
