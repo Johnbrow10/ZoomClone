49d67d73b0261f6558e490ab1c1cb51f36ef35b5
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-26 15:51:02 -0300

    Criando server, configurando o cors e o socket.io

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const server = require('http').createServer((request, response) => {
+    response.writeHead(204, {
+        'access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',   
+
+    })
+    response.end('Server teve uma requisição')
+})
+
+const socketIo = require('socket.io');
+const io = socketIo(server, {
+    cors: {
+        origin: '*',
+        creadetials: false
+    }
+})
+
+io.on('connection', socket => {
+    console.log('connection', socket.id);
+    socket.on('join-room', (roomId,userId) =>{
+        // Adicionar os usuariosna mesma sala
+        socket.join(roomId)
+        // notificar todos na sala que usuario usuario novo entrou
+        socket.to(roomId).broadcast.emit('usuario conectou', userId)
+        // notificar que o usuario saiu da sala
+        socket.on('disconnect', ()=> {
+            console.log('disconected!', roomId, userId)
+            socket.to(roomId).broadcast.emit('usuario desconectou',userId)
+
+        })
+    })
+})
