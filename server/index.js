27f17589557a8caa369438147285db6be876501e
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-26 20:33:45 -0300

    Mandando mensagens ao se conectar e desconetar

--- server/index.js
+++ server/index.js
@@ -1,38 +1,44 @@
+// Criando um servidor para acessar o cors atraves do http do JavaScript
+
 const server = require('http').createServer((request, response) => {
+    // dando autorização para os metodos e verbos em http
     response.writeHead(204, {
-        'access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',   
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
 
     })
     response.end('Server teve uma requisição')
 })
 
+// ultilizando a biblioteca em socket para acessar o servidor ultilizando as credenciais 
 const socketIo = require('socket.io');
 const io = socketIo(server, {
     cors: {
         origin: '*',
-        creadetials: false
+        credentials: false
     }
 })
 
 io.on('connection', socket => {
     console.log('connection', socket.id);
-    socket.on('join-room', (roomId,userId) =>{
+    // Quando o usuario logar ele terar que entrar em uma sala expecifica
+    socket.on('join-room', (roomId, userId) => {
         // Adicionar os usuariosna mesma sala
         socket.join(roomId)
         // notificar todos na sala que usuario usuario novo entrou
-        socket.to(roomId).broadcast.emit('usuario conectou', userId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
         // notificar que o usuario saiu da sala
-        socket.on('disconnect', ()=> {
+        socket.on('disconnect', () => {
             console.log('disconected!', roomId, userId)
-            socket.to(roomId).broadcast.emit('usuario desconectou',userId)
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
 
         })
     })
 })
 
+// startando o servidor com a porta e o endereço 
 const startServer = () => {
-    const {address, port} = server.address()
+    const { address, port } = server.address()
     console.info(`Aplicação rodando em ${address}:${port}`)
 }
 
