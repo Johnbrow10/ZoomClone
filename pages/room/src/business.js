2eb7b4fac574017e8fe609556fd863cda7363d6c
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-26 15:27:30 -0300

    praparando as responsabilidades da view e businnes e uando media para ultilizar a camera do navegador.

---
+++ pages/room/src/business.js
@@ -0,0 +1,31 @@
+class Business {
+    constructor({ room, media, view }) {
+        this.media = media;
+        this.room = room;
+        this.view = view;
+
+        this.currentStream = {}
+    }
+
+    static initialize(deps) {
+        const instance = new Business(deps);
+        return instance._init();
+    }
+
+    async _init() {
+        this.currentStream = await this.media.getCamera();
+        // console.log('init!!', this.currentStream)
+
+        this.addVideoStream('teste01');
+
+    }
+
+    addVideoStream(userId, stream = this.currentStream) {
+        const isCurrentId = false;
+        this.view.renderVideo({
+            userId,
+            stream,
+            isCurrentId
+        })
+    }
+}
