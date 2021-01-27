c657dd00bbd070b9e38f2123822197302b87bf77
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-27 17:20:19 -0300

    Controller business com todas as funções do peer.js setada para ultilizar as chamadas com mias de uma camera ligada.

--- pages/room/src/business.js
+++ pages/room/src/business.js
@@ -1,17 +1,20 @@
 class Business {
 
-    constructor({ room, media, view, socketBuilder }) {
+    constructor({ room, media, view, socketBuilder, peerBuilder }) {
         this.media = media;
         this.room = room;
         this.view = view;
 
-        // se o usuario estiver conetado ele faz a build para o servidor que estar rodando por trás 
-        this.socketBuilder = socketBuilder
-            .setOnUserConnected(this.onUserConnected())
-            .build();
+        this.peerBuilder = peerBuilder;
 
-        this.socketBuilder.emit('join-room', this.room, 'teste01')
-        this.currentStream = {}
+        // se o usuario estiver conetado ele faz a build para o servidor que estar rodando por trás 
+        this.socketBuilder = socketBuilder;
+
+        this.currentStream = {};
+        this.socket = {};
+        this.currentPeer = {};
+
+        this.peers = new Map();
     }
 
     // inicializa o controlador businnes com as dependencias e intanciando elas no app.js
@@ -23,8 +26,20 @@ class Business {
     // aqui a função init anste de tudo pede permisão do usuario e se
     // ele aceita ela roda a função para acessar a camera do navegador
     async _init() {
-        this.currentStream = await this.media.getCamera(true);
-        console.log('init!!', this.currentStream)
+
+        this.currentStream = await this.media.getCamera();
+        this.socket = this.socketBuilder
+            .setOnUserConnected(this.onUserConnected())
+            .setOnUserDisconnected(this.onUserDisconnected())
+            .build();
+
+        this.currentPeer = await this.peerBuilder
+            .setOnError(this.onPeerError())
+            .setOnConnectionOpened(this.onPeerConnectionOpened())
+            .setOnCallReceived(this.onPeerCallReceived())
+            .setOnPeerStreamReceived(this.onPeerStreamReceived())
+            .build()
+
 
         this.addVideoStream('teste01');
 
@@ -44,13 +59,48 @@ class Business {
     onUserConnected = function () {
         return userId => {
             console.log('user connected', userId);
+            this.currentPeer.call(userId, this.currentStream)
         }
+
     }
 
     onUserDisconnected = function () {
         return userId => {
             console.log('user disconnected', userId);
         }
+    }
+
+    onPeerError = function () {
+        return error => {
+            console.error('error on peer', error);
+        }
+    }
+
+    onPeerConnectionOpened = function () {
+        return (peer) => {
+            console.log('peer ta vivo', peer)
+            const id = peer.id
+            this.socket.emit('join-room', this.room, id);
+
+        }
+    }
+
+    onPeerCallReceived = function () {
+        return call => {
+            console.log("respondendo uma chamada", call)
+            call.answer(this.currentStream)
+        }
+    }
+
+    onPeerStreamReceived = function () {
+        return (call, stream) => {
+            const callerId = call.peer;
+            this.addVideoStream(callerId, stream)
+            this.peers.set(callerId, { call })
+
+            this.view.setParticipantes(this.peers.size)
+        }
+
     }
 
 }
