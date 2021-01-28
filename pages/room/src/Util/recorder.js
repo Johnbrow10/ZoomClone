ec52a5cd2c4be228cbd99f8e59adf2c955502d9c
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-28 20:00:00 -0300

    Criação do recordJs para configurar o MediaRecord atraves do mediaRecordes ele acessa os codecs do browse e gera o webm.

---
+++ pages/room/src/Util/recorder.js
@@ -0,0 +1,60 @@
+class Recorder {
+    constructor(userName, stream) {
+        this.userName = userName;
+        this.stream = stream;
+        this.filename = `id:${userName}-when:${Date.now()}`;
+        this.videoType = 'video/webm';
+
+        this.mediaRecorder = {};
+        this.recordedBlobs = [];
+        this.recordingActive = false;
+    }
+
+    _setup() {
+        const commonCodecs = [
+            "codecs=vp9,opus",
+            "codecs=vp8,opus",
+            "",
+
+        ]
+
+        const options = commonCodecs
+            .map(codec => ({ mimeType: `${this.videoType};${codec}` }))
+            .find(options => MediaRecorder.isTypeSupported(options.mimeType))
+
+        if (!options) {
+            throw new Error(`não tem codecs para esse browser: ${commonCodecs.join(',')} are suported`)
+        }
+
+        return options;
+    }
+
+    startRecording() {
+
+        const options = this._setup();
+        console.log("startou a gravação", this.userName, this.filename)
+
+        // se nao tiver mais video nem grava, ja ignora!!
+        if (!this.stream.active) return;
+        this.mediaRecorder = new MediaRecorder(this.stream, options)
+        console.log(`Criou MediaRecorder ${this.MediaRecorder} e essas são as opções ${options}`)
+
+        this.mediaRecorder.onstop = (event) => {
+            console.log("record blobs que foram gravados", this.recordedBlobs);
+
+        }
+
+        this.mediaRecorder.ondataavailable = (event) => {
+            // toda vez que o navegador conseguir ir gravando ele ira jogar todos os binarios dentro desse array
+            if (!event.data || !event.data.size) return;
+
+            this.recordedBlobs.push(event.data)
+        }
+
+        this.mediaRecorder.start();
+        console.log(`Media record iniciou!!`, this.mediaRecorder);
+        this.recordingActive = true
+
+
+    }
+}
