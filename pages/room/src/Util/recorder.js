58f330b65c8807227231b40e865815d20357b0e9
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-29 17:52:33 -0300

    fucao download criar elemento a para baixar as gravacoes quando apertar o botao end e sair da reunião

--- pages/room/src/Util/recorder.js
+++ pages/room/src/Util/recorder.js
@@ -82,4 +82,20 @@ class Recorder {
         })
     }
 
+    download() {
+        if (!this.completeRecordings.length) return;
+
+        for (const recording of this.completeRecordings) {
+            const blob = new Blob(recording, { type: this.videoType })
+            const url = window.URL.createObjectURL(blob)
+            const a = document.createElement('a')
+            a.style.display = 'none'
+            a.href = url
+            a.download = `${this.filename}.webm`
+            document.body.appendChild(a)
+            a.click()
+        }
+
+    }
+
 }
